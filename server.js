046cfc497b2d22d8bd0b762e88a86fd43a7163af
edean78 <edean78@gmail.com>
2019-12-03 11:33:44 -0500

    Added express, express-handlebars, mysql as dependencies. Added code to handle data parsing and handlebars.

--- server.js
+++ server.js
@@ -1,2 +1,23 @@
+// Create delendencies
 var express = require('express');
+var exphbs = require('express-handlebars');
+var mysql = require('mysql');
 
+var app = express();
+
+// Set the the port to be used by your app
+// process.env.PORT lets the port be set by heroku
+var PORT = process.env.PORT;
+
+// Set up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up the application to use handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set("view engine", "handlebars");
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
