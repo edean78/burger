4531505b5c0cfbb1d9d95af35a31a02a45d104f3
edean78 <edean78@gmail.com>
2019-12-04 22:10:03 -0500

    Created the orm.js file with all of the SQL statments

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,107 @@
-var connections = require("connection.js");
+// Import MySQL connection
+var connection = require("../config/connectionjs");
+
+// Helper function for SQL syntax
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++){
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function odjToSql(ob) {
+    var arr = [];
+
+    // loop trhough the keys and push the key/value as a string int arr
+    for(var key in ob) {
+        var value = ob[key];
+        // check to skip hidden prperties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Ed Dean => 'Ed Dean')
+            if (typeof value === "string" && value.indexOf(" ") >= 0){
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single conna-seperated string
+    return arr.toString();
+}
+
+// Object for all our SQL statment functions.
+var orm = {
+    selectAll: function(tableInput, cb){
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    createBurger: function(table, cols, vals, cb){
+        var queryString = "INSERT INTO " + table;
+        
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+
+    delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+// Export the orm object for the model
+module.exports = orm;
