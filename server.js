b50bf473431ed06e75f59ab54fbe763d652fefda
edean78 <edean78@gmail.com>
2019-12-04 22:46:08 -0500

    Imported the routes by requiring the burger_controller.js file

--- server.js
+++ server.js
@@ -17,8 +17,13 @@ app.use(express.json());
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set("view engine", "handlebars");
 
+// Import routes and give ther server access to them
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
-  });
+});
