ca2eca80976b982e2bd24b8f23de1951fa272075
edean78 <edean78@gmail.com>
2019-12-08 00:49:02 -0500

    Updated burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -5,7 +5,7 @@ $(function(){
         var newDevour = $(this).data("newdevour");
 
         var newDevourState = {
-            devoured: newDevour
+            isDevoured: newDevour
         };
 
         // Send the PUT request.
@@ -38,21 +38,6 @@ $(function(){
                 console.log("creaeted new burger");
                 // Reload the page to get the updated list
                 location.reload();
-            }
-        );
-    });
-
-    $(".delete-burger").on("click", function(event){
-        var id = $(this).data("id");
-
-        // Send the DELETE request.
-        $.ajax("/api/burgers/" + id, {
-            type: "DELETE"
-        }).then(
-            function() {
-                console.log("deleted burger");
-                // Reload the page to get the updated list
-                locations.reload();
             }
         );
     });
