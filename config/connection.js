62f08afd821d4b46484e4f0e50d8559580e4e5e0
edean78 <edean78@gmail.com>
2019-12-07 21:58:38 -0500

    Created the JAWSDB in heroku then updated the config file

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 // Set up MySQL connection
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "docker",
-    database: "burger_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "docker",
+//     database: "burger_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "docker",
+        database: "burger_db",
+        port: 3306
+    });
+};
 
 // Make connection
 connection.connect(function(err){
